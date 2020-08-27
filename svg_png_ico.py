8c30b32bdf613934da4beac955ac19cdf26677fd
navercm418 <51379254+navercm418@users.noreply.github.com>
2020-08-27 16:09:07 -0400

    Update svg_png_ico.py

--- svg_png_ico.py
+++ svg_png_ico.py
@@ -2,14 +2,15 @@ import sys
 import subprocess
 from PIL import Image
 
-filename = sys.argv[1]
-savefile = sys.argv[2]
-ht = int(sys.argv[3])
-wt = int(sys.argv[4])
+svg_file = sys.argv[1]
+png_file = sys.argv[2]
+ico_file = sys.argv[3]
+ht = int(sys.argv[4])
+wt = int(sys.argv[5])
 
 # convert -background none a.svg b.png
-cnv = subprocess.Popen(["convert", "-background", "none", filename, savefile])
+cnv = subprocess.Popen(["convert", "-background", "none", svg_file, png_file])
 cnv.wait()
 
-img = Image.open(savefile)
-img.save(savefile +'.ico',format = 'ICO', sizes=[(ht,wt)])
+img = Image.open(png_file)
+img.save(ico_file, format = 'ICO', sizes=[(ht, wt)])
